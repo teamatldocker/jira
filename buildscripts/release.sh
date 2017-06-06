e208cf5c6f9350e93289763fa771db8f2d083155
Grant Slater <grant.slater@wearefriday.com>
2017-06-06 15:55:23 +0100

    JIRA + Service Desk version bump
    JIRA 7.3.6 -> 7.3.7
    JIRA Service Desk 3.5.0 -> 3.5.1

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,5 +3,5 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JIRA_VERSION=7.3.6
-export JIRA_SERVICE_DESK_VERSION=3.5.0
+export JIRA_VERSION=7.3.7
+export JIRA_SERVICE_DESK_VERSION=3.5.1
