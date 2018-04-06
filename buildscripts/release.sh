07791844533f7f8e547a4944509ce8f2b0924159
Grant Slater <grant.slater@wearefriday.com>
2018-04-06 11:26:29 +0100

    Update to JIRA 7.8.2
    JIRA Release notes: https://confluence.atlassian.com/jirasoftware/jira-software-7-8-x-release-notes-945521251.html
    Service Desk Release notes: https://confluence.atlassian.com/servicedesk/jira-service-desk-3-11-x-release-notes-945120384.html

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,6 +3,6 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JIRA_VERSION=7.8.1
-export JIRA_SERVICE_DESK_VERSION=3.11.1
+export JIRA_VERSION=7.8.2
+export JIRA_SERVICE_DESK_VERSION=3.11.2
 export JIRA_DEVELOPMENT_TAG=development
