c505a492ce215b67d88e81db526eec63e5bc4eae
Nikolai Bezruk <nikolai@kingmuffin.com>
2018-05-10 17:51:29 +0300

    Jira 7.9.2 and Service Desk 3.12.2

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,6 +3,6 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JIRA_VERSION=7.9.1
-export JIRA_SERVICE_DESK_VERSION=3.12.1
+export JIRA_VERSION=7.9.2
+export JIRA_SERVICE_DESK_VERSION=3.12.2
 export JIRA_DEVELOPMENT_TAG=development
