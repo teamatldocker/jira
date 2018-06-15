63424c95ac6a43595641c6e0592cb5978ab2c02d
Jonathan Hult <jonathan@jonathanhult.com>
2018-06-15 09:10:39 -0400

    Jira 7.10.1 and Service Desk 3.13.1

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,6 +3,6 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JIRA_VERSION=7.10.0
-export JIRA_SERVICE_DESK_VERSION=3.13.0
+export JIRA_VERSION=7.10.1
+export JIRA_SERVICE_DESK_VERSION=3.13.1
 export JIRA_DEVELOPMENT_TAG=development
