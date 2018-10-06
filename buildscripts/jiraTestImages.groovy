c781e80554c74aef0db8a44d1363b79fe92efed3
Steffen Bleul <sbl@itbleul.de>
2018-10-06 09:51:27 +0200

    Implementing circle-ci workflow.

--- buildscripts/jiraTestImages.groovy
+++
@@ -1,9 +0,0 @@
-node('docker') {
-    checkout scm
-    sh './buildscripts/release.sh && ./buildscripts/cleanJiraContainers.sh'
-    try {
-      sh './buildscripts/release.sh && ./buildscripts/testSupportedJiraImages.sh'
-    } finally {
-      sh './buildscripts/release.sh && ./buildscripts/cleanJiraContainers.sh'
-    }
-}
