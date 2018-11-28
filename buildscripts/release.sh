785a796b6496f545df7c18377d1c0ded93089e4f
Grant Slater <grant.slater@wearefriday.com>
2018-11-28 17:08:36 +0000

    Upgrade to JIRA 7.13.0 (Enterprise Release)
    Release Notes: https://confluence.atlassian.com/jirasoftware/jira-software-7-13-x-release-notes-957981568.html
    Service Desk Release Notes: https://confluence.atlassian.com/servicedesk/jira-service-desk-3-16-x-release-notes-957507088.html

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,6 +3,6 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JIRA_VERSION=7.12.3
-export JIRA_SERVICE_DESK_VERSION=3.15.3
+export JIRA_VERSION=7.13.0
+export JIRA_SERVICE_DESK_VERSION=3.16.0
 export JIRA_DEVELOPMENT_TAG=development
