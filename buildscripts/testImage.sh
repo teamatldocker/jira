4bb9f44775bf722a3803bad4576a71c84985ccd4
Jonathan Hult <Jonathan@JonathanHult.com>
2019-07-24 09:46:29 -0400

    build test script - remove while; use curl retry

--- buildscripts/testImage.sh
+++ buildscripts/testImage.sh
@@ -8,24 +8,14 @@ testImage() {
   local networkName="jira_dockertestnet"
   docker run --rm -d --network $networkName --name="$containerName" teamatldocker/jira:"$tag"
 
-  local iteration=0
-  while true; do
-    local response
-    set +e
-    # CircleCI does not (easily) allow exposing Docker ports so we always use port 8080
-    response=$(docker run --rm --network $networkName byrnedo/alpine-curl -s -o /dev/null -I -w '%{http_code}' http://"$containerName":8080)
-    set -e
-    if [[ $response == 2* ]] || [[ $response == 3* ]]; then
-      break
-    elif [[ $iteration == 10 ]]; then
-      exit 1
-    else
-      ((iteration++))
-      echo "HTTP status of iteration $iteration: $response"
-      echo "Will wait and retry ..."
-    fi
-    sleep 10
-  done
+  local response
+  set +e
+  # CircleCI does not (easily) allow exposing Docker ports so we always use port 8080
+  response=$(docker run --rm --network $networkName byrnedo/alpine-curl -s -o /dev/null -I -w '%{http_code}' --retry-connrefuse --max-time 10 --retry 5 --retry-delay 20 --retry-max-time 180 http://"$containerName":8080)
+  set -e
+  if [[ $response != 2* ]] && [[ $response != 3* ]]; then
+    exit 1
+  fi
   docker stop "$containerName"
 
 }
